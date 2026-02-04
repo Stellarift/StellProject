7d56c9f8d52e92b9b547d0ce659836a47e09f7f8
Stellarift <filippov2888@gmail.com>
2026-02-04 16:14:03 +0900

    Update menu.h

--- menu.h
+++ menu.h
@@ -4,16 +4,16 @@
 
 #include "task.h"
 
-void showTask(int i);
-void showAllTasks();
-void addTask();
-void deleteTask();
-void editTask();
-void searchTasks();
-void showTasksByPeriod();
-void sortTasks();
-void deleteAllTasks();
-void showAbout();
-void showMenu();
+void showTask(int i); // Показать одну задачу (вспомогательная) 
+void showAllTasks(); // Показать все задачи
+void addTask(); // Добавляет задачу
+void deleteTask(); // Удалить одну задачу
+void editTask(); // Редактировать существующую задачу
+void searchTasks(); //  Поиск задач по разным критериям
+void showTasksByPeriod(); // Показать задачи на конкретную дату
+void sortTasks(); //  Сортировать задачи (по приоритету/дате/статусу) 
+void deleteAllTasks(); // Удалить все задачи (очистить список) 
+void showAbout(); //  Показать информацию о программе
+void showMenu(); // Главное меню программы (управляющая функция) 
 
 #endif
